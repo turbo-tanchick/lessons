8a5880abcfd700cd9c9c7d827d87f3eb980561a4
Tatiana Rud <97253344+turbo-tanchick@users.noreply.github.com>
2024-01-18 23:50:05 +0200

    Add files via upload

---
+++ lesson_24/js/script.js
@@ -0,0 +1,392 @@
+"use strict"
+
+/* let userAge = 35;
+
+console.log('привіт')
+console.log(userAge)
+userAge = "Tanya"
+console.log(userAge) */
+
+/* let userAge = 25
+console.log(userAge)
+let OtherUserAge = 45
+console.log(OtherUserAge)
+OtherUserAge = userAge
+console.log(OtherUserAge)
+console.log(userAge) */
+
+//console.log(userAge);
+/* let userAge = 35;
+console.log(userAge); */
+
+/* const userName = 'Tanya'
+console.log(userName);
+
+const userAge = 35
+console.log(userAge); */
+
+//let userAge;
+///console.log(typeof userAge);
+//console.log(userAge);
+/* if (typeof userAge === "undefined"){
+console.log('Все пропало!');
+} */
+
+/* let userAge = null;
+console.log(typeof userAge);
+console.log(userAge); */
+
+/* let userAge=35;
+console.log(userAge); */
+
+/* let result = 100 / 0;
+console.log(typeof result);
+console.log(result); */
+
+/* let result = "Tanya" * 7;
+console.log(typeof result);
+console.log(result); */
+
+/* let myAge = `35`;
+let myName = `Tanya`;
+console.log(typeof myName);
+console.log(myName);
+console.log(typeof myAge);
+console.log(myAge); */
+
+/* let myAge = `35`;
+let myName = `Tanya`;
+let aboutMe = `Ім'я: ${myName}, вік: ${myAge}`;
+console.log(aboutMe); */
+
+//let someVar = ``;
+/* console.log(someVar);
+console.log(typeof someVar);
+console.log(typeof Boolean (someVar));
+console.log(Boolean(someVar)); */
+
+/* if (someVar){
+  console.log(`Good luck`);
+} */
+
+/* let someString = `Привіт! Моє ім'я Тетянка!`
+console.log(someString.length);
+console.log(someString[17]);
+console.log(someString.toUpperCase());
+console.log(someString.toLowerCase());
+console.log(someString.includes(`Моє`));
+console.log(someString.startsWith(`Прив`));
+console.log(someString.endsWith(`.`));
+let someNewString = someString.slice(1, 7);
+console.log(someNewString);
+someString = someString.replace(`Тетянка`, `Іванка`);
+console.log(someString);
+ */
+
+/* let someNumb = 48.7;
+console.log(Math.floor (someNumb));
+console.log(Math.ceil (someNumb));
+someNumb = 24.2;
+console.log(Math.round(someNumb));
+someNumb = -24.2;
+console.log(Math.abs(someNumb));
+console.log(Math.random())
+console.log(Math.max(3, 7, -12));
+console.log(Math.min(3, 7, -12));
+let numMax = Math.max(3, 7, 78, -12);
+console.log(numMax); */
+
+//let someVar = `35.5px`;
+/* let someNum = parseInt (someVar);
+console.log(someNum); */
+/* let someNum = parseFloat(someVar);
+console.log(someNum); */
+
+/* let varOne = "17";
+let varTwo = "2"; */
+/* let varRes = varOne - varTwo;
+console.log(typeof varRes);
+console.log(varRes); */
+
+/* let varRes = varOne / varTwo;
+console.log(typeof varRes);
+console.log(varRes); */
+
+/* let varRes = varOne * varTwo;
+console.log(typeof varRes);
+console.log(varRes); */
+
+/* let varRes = varOne % varTwo;
+console.log(typeof varRes);
+console.log(varRes); */
+
+/* let varSumm = varOne + varTwo;
+console.log(typeof varSumm);
+console.log(varSumm); */
+
+/* let varOne = +"17";
+let varTwo = +"2";
+let varSumm = varOne + varTwo;
+console.log(typeof varSumm);
+console.log(varSumm); */
+
+/* let someNumOne = 9;
+let someNumTwo = 89;
+++someNumOne;
+--someNumTwo;
+console.log(someNumOne);
+console.log(someNumTwo); */
+
+/* let result = 2 + 3 * 10 / + "5";
+console.log(result); */
+
+/* let num = 5;
+let results = 2 + 3 * 10 / ++num;
+console.log(results); */
+
+/* let result = 5 >= 6
+console.log(typeof result);
+console.log(result); */
+
+/* let result = 5 == "5"
+console.log(result); */
+
+/* let result = 5 != "5"
+console.log(result); */
+
+/* let result = 5 === "5"
+console.log(result); */
+
+/* let result = 5 !== "5"
+console.log(result); */
+
+/* let a = 5
+let b = "5"
+let result = a === b;
+console.log(result); */
+
+/* let result = !0;
+console.log(typeof result);
+console.log(result); */
+
+/* let result = 0 || 5;
+console.log(result);
+console.log(Boolean(result)); */
+
+/* let result = 7 && 0;
+console.log(result);
+console.log(Boolean(result)); */
+
+/* let varOne = 5;
+let varTwo = 10;
+if (varOne > varTwo) {
+  console.log(`${varOne} більше ніж ${varTwo}`);
+} else if (varOne === 10) {
+  console.log(`varOne дорівнює 10`);
+} else if (varTwo === 20) {
+  console.log(`varTwo дорівнює 20`);
+} else {
+  console.log(`Все пропало...`);
+}
+console.log('Продовження коду...'); */
+/* 
+let varOne = "1";
+let varTwo = 3;
+let varThree = 3;
+let varFour = 7;
+
+if (varTwo === 1 || varOne > 3 && varThree < 4 || varFour > 6) {
+  console.log('Умова true');
+} */
+
+/* let someVar = 10 > 5 ? "10 більше 5" : "10 меньше 5";
+console.log(someVar); */
+
+/* let someVar;
+if (10 > 5) {
+  someVar = "10 більше 5";
+} else {
+  someVar = "10 меньше 5";
+}
+console.log(someVar); */
+
+/* let someVar = "";
+let someNewVar = someVar ? "Так, все ок" : null;
+console.log(someNewVar); */
+
+/* let someString = `Привіт! Моє ім'я Тетянка!`;
+console.log(someString.length); */
+/* console.log(someString[0]);
+console.log(someString[1]);
+console.log(someString[2]);
+console.log(someString[3]);
+console.log(someString[4]);
+console.log(someString[5]);
+console.log(someString[6]); */
+/* for (let i = 0; i < someString.length; ++i) {
+  console.log(someString[i]);
+} */
+
+/* let someArray = [];
+console.log(typeof someArray);
+console.log(Array.isArray(someArray)); */
+//let someArray = [true, 45, `tetyana`, "Привіт!"];
+/* console.log(someArray);
+console.log(someArray[2]);
+console.log(someArray.length); */
+/* 
+for (let i = 0; i < someArray.length; ++i) {
+  console.log(i);
+  console.log(someArray[i]);
+} */
+
+/* let someArray = [true, 45, `tetyana`, "85"];
+someArray.forEach((value, index) => {
+  console.log(index);
+  console.log(value);
+  if (typeof value === "string") {
+    for (let i = 0; i < value.length; ++i) {
+      console.log(value[i]);
+    }
+  }
+}) */
+
+/* let usersNames = ["Вася", "Петро", "Ілдика"];
+console.log(usersNames);
+usersNames.push("Жека");
+console.log(usersNames); */
+
+//let usersNames = ["Вася", "Петро", "Ілдика"];
+/* console.log(usersNames);
+console.log(usersNames.includes("Петро"));
+if (!usersNames.includes(10)) {
+  usersNames.push(10);
+}
+console.log(usersNames); */
+
+/* !usersNames.includes(10) ? usersNames.push(10) : null;
+console.log(usersNames); */
+
+/* let usersNames = ["Вася", "Петро", "Ілдика"];
+let usersNamesStr = usersNames.join();
+console.log(usersNamesStr); */
+
+/* function showMessage() {
+  console.log('hello!');
+}
+showMessage() */
+
+/* function showMessage(someText = "Текст за замовченням") {
+  console.log(someText);
+}
+showMessage()
+showMessage("Все ок, тримаємось!") */
+
+/* function calcSum(a = 0, b = 0) {
+  const result = a + b;
+  showMessage(result);
+}
+function showMessage(someText = "Текст за замовченням") {
+  console.log(someText);
+}
+calcSum(10, 30); 
+calcSum(50, 100)
+calcSum(50, -320)
+calcSum(10) */
+
+/* function calcSum(a = 0, b = 0) {
+  return a + b
+}
+function showMessage(someText = "Текст за замовченням") {
+  console.log(someText);
+} */
+/* showMessage(calcSum(10, 20)) */
+/* let result = calcSum(10, 20);
+showMessage(result); */
+
+/* let someVar;
+let someFuncVar;
+function someFunc() {
+  someVar = 10;
+  someFuncVar = function (message) {
+    console.log(message);
+  }
+}
+someFunc()
+someFuncVar("Привіт!")
+console.log(someVar); */
+
+/* let some = (text) => console.log(text);
+some(`hello`) */
+
+/* let someText = "";
+console.log(someText);
+someText = someText + "<div></div>";
+console.log(someText);
+someText = someText + `<a href="contacts.html"></a>`;
+console.log(someText); */
+/* let someText = "";
+someText += `<div>`
+someText += `<a href="contacts.html"></a>`
+someText += `</div>`
+console.log(someText); */
+
+//Домашнє завдання
+//Завдання № 1
+let someVar = 0;
+++someVar;
+
+if (someVar) {
+  console.log(someVar);
+}
+/* В консоль потрапить цифра 1, тому що Інкремент збільшить на 1 цифру 0 */
+
+// Завдання № 2
+let someArray = ["Пункт №1", "Пункт №2", "Пункт №3", "Пункт №4", "Пункт №5", "Пункт №6", "Пункт №7", "Пункт №8", "Пункт №9", "Пункт №10"]
+
+for (let i = 0; i < someArray.length; ++i) {
+  console.log(someArray[i]);
+} 
+
+//Завдання № 3
+
+if (2 * 20 <= 10 || 30 / 2 < 5 && 10 <= "10" || 20 === "20") {
+  console.log('000');
+}
+
+// Крок № 1 множення та ділення
+/* if (40 <= 10 || 15 < 5 && 10 <= "10" || 20 === "20") {
+} */
+// Крок №2 (Пріорітет у операторів порівняння)
+//if (false || false && true || false) {}
+
+// Крок №3 (Пріорітет у оператора &&)
+//if (false || false || false) {}
+
+// Крок №3 (Пріорітет у оператора ||)
+// if (false) {}
+
+//В консоль нічого не потрапить, тому що код виконоється, тільки якщо умова вертає true
+
+function calcSum(a = 0, b = 0) {
+  return "Результат ділення: " + +a / +b;
+}
+let result = calcSum(120, 7);
+console.log(result);
+
+//Завдання № 5
+
+let numArray = ["hello", true, "Tetyana", 10, 96];
+
+numArray.forEach((value, index) => {
+  console.log(index);
+  console.log(value);
+  if (typeof value === "number") {
+    for (let i = 0; i < value.length; ++i) {
+      console.log(value[i]);
+    }
+  }
+})
+
+console.log(numArray.includes(10));
+console.log(numArray);
